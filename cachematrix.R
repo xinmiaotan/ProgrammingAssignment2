9f63f7d5b3f0708ec358f8f9cf488229e67da071
xinamiaotan <xinmiaotan@gmail.com>
2018-05-29 14:29:00 -0700

    Edit the R
    Edit the R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#ProgrammingAssignment2
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inverse) m <<- inverse
+    getInv <- function() m
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    if(!is.null(m)) {
+        message("getting cached Invertible Matrix")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInv(m)
+    m
 }
+
+Test1 <- matrix(1:4,2,2)
+Test1
+
+CacheM <- makeCacheMatrix(Test1)
+CacheM$get()
+
+Test2 <- cacheSolve(CacheM)
+Test2
+solve(Test1) 
+
+Test1 %*% Test2
+
+Test1 <- rbind(c(1,-1/4),c(-1/4,1))
+Test1
+CacheM <- makeCacheMatrix(Test1)
+CacheM$get()
+
+
+Test2 <- cacheSolve(CacheM)
+Test2
+solve(Test1)
+
+Test1 %*% Test2
